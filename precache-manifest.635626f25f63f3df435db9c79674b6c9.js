a40d6fe41933fa17cb08e48ef486e1aed4609b3e
Vikram Singh <58028680+bcalm@users.noreply.github.com>
2020-10-10 13:58:52 +0530

    Updates

---
+++ precache-manifest.635626f25f63f3df435db9c79674b6c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f14e262ccd1884da3ecfc6100974624",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "be343736641b3205f052",
+    "url": "/todo-react/static/css/main.be12c345.chunk.css"
+  },
+  {
+    "revision": "ab6aa2521d50010f1649",
+    "url": "/todo-react/static/js/2.808a1785.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.808a1785.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be343736641b3205f052",
+    "url": "/todo-react/static/js/main.30831e38.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
